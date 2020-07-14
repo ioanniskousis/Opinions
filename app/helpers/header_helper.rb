a2ea392ddb54589f1341483ab148c143f623edd2
Ioannis Kousis <bonobo@otenet.gr>
2020-07-14 18:24:21 +0300

    first approach for desktop images on cloud

---
+++ app/helpers/header_helper.rb
@@ -0,0 +1,9 @@
+module HeaderHelper
+  def opinions_image
+    'https://storage.cloud.google.com/opinions-bucket/images/opinions.png'
+  end
+
+  def pencil_image
+    'https://storage.cloud.google.com/opinions-bucket/images/pencil.png'
+  end
+end
